c0b6dce7d9a036c28d3b7d1e5e40dbc6b2bfd3cb
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:41:31 +0800

    test: expose 源方法返回值未参与复制

---
+++ goletalab_017_test.go
@@ -0,0 +1,12 @@
+package copier
+
+import ("fmt";"reflect";"strings";"testing")
+
+var _=fmt.Sprint
+var _=strings.Contains
+var _=reflect.DeepEqual
+type goletaMethodSource017 struct{Base int}
+func (s goletaMethodSource017) Score()int{return s.Base*2}
+type goletaMethodDest017 struct{Score int}
+
+func TestGoletaCopier017(t *testing.T){src:=goletaMethodSource017{Base:6};var dst goletaMethodDest017;if err:=Copy(&dst,src);err!=nil{t.Fatal(err)};if dst.Score!=12{t.Fatalf("score=%d",dst.Score)}}
