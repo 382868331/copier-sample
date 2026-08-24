4654e0b13993a4c87739e4c9e9efade460a7ef9f
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:41:00 +0800

    fix: restore 同类型切片首项遗漏

--- goletalab_015_test.go
+++ goletalab_015_test.go
@@ -7,3 +7,10 @@ var _=strings.Contains
 var _=reflect.DeepEqual
 
 func TestGoletaCopier015(t *testing.T){src:=[]int{4,7,9};var dst []int;if err:=Copy(&dst,src);err!=nil{t.Fatal(err)};if !reflect.DeepEqual(dst,src){t.Fatalf("dst=%v",dst)}}
+
+func TestGoletaCopier015Boundary(t *testing.T) {
+ src:=[]string{"first"}
+ var dst []string
+ if err:=Copy(&dst,src);err!=nil{t.Fatal(err)}
+ if !reflect.DeepEqual(dst,src){t.Fatalf("dst=%v",dst)}
+}
