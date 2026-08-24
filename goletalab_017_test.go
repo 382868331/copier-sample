37147b91d127fba45bf3a77867e8f99a53e8fc11
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:41:37 +0800

    fix: restore 源方法返回值未参与复制

--- goletalab_017_test.go
+++ goletalab_017_test.go
@@ -10,3 +10,10 @@ func (s goletaMethodSource017) Score()int{return s.Base*2}
 type goletaMethodDest017 struct{Score int}
 
 func TestGoletaCopier017(t *testing.T){src:=goletaMethodSource017{Base:6};var dst goletaMethodDest017;if err:=Copy(&dst,src);err!=nil{t.Fatal(err)};if dst.Score!=12{t.Fatalf("score=%d",dst.Score)}}
+
+func TestGoletaCopier017Boundary(t *testing.T) {
+ src:=goletaMethodSource017{Base:-3}
+ var dst goletaMethodDest017
+ if err:=Copy(&dst,src);err!=nil{t.Fatal(err)}
+ if dst.Score!=-6{t.Fatalf("score=%d",dst.Score)}
+}
